d440441c511888c9e8fb6622d19f213b8dd80c4d
Garetonchick <dsbonich@edu.hse.ru>
2025-01-01 15:53:26 +0100

    moving with arrow keys window demo

--- komaru/editor/main.cpp
+++ komaru/editor/main.cpp
@@ -1,8 +1,71 @@
+#include <cmath>
 #include <glad/glad.h>
 #include <imgui/imgui.h>
 #include <imgui/backends/imgui_impl_glfw.h>
 #include <imgui/backends/imgui_impl_opengl3.h>
 #include <GLFW/glfw3.h>
+
+
+const float EPS = 1e-5;
+
+bool IsZeroLen(ImVec2 v) {
+    return (v.x * v.x + v.y * v.y) < EPS * EPS;
+}
+
+class TestApp {
+public:
+    TestApp(GLFWwindow* window) : window_(window) {}
+
+    void Draw() {
+    }
+
+    void Update(float dt) {
+        ImGui::SetNextWindowSize(ImVec2(200, 200), ImGuiCond_Once);
+        ImGui::SetNextWindowPos(win_pos_, ImGuiCond_Once);
+
+        ImGui::Begin("Test window");
+
+        auto dir = GetDir();
+        if(!IsZeroLen(dir)) {
+            win_pos_.x += dir.x * speed_ * dt;
+            win_pos_.y += dir.y * speed_ * dt;
+        } else {
+            win_pos_ = ImGui::GetWindowPos();
+        }
+
+        ImGui::SetWindowPos(win_pos_, ImGuiCond_Always);
+
+        ImGui::End();
+    }
+
+private:
+    ImVec2 GetDir() {
+        ImVec2 dir;
+        if (glfwGetKey(window_, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+           dir.x += 1.0;
+        }
+        if (glfwGetKey(window_, GLFW_KEY_LEFT) == GLFW_PRESS) {
+            dir.x -= 1.0;
+        }
+        if (glfwGetKey(window_, GLFW_KEY_DOWN) == GLFW_PRESS) {
+            dir.y += 1.0;
+        }
+        if (glfwGetKey(window_, GLFW_KEY_UP) == GLFW_PRESS) {
+            dir.y -= 1.0;
+        }
+        float len = sqrt(dir.x * dir.x + dir.y * dir.y);
+        if(len > EPS) {
+            dir.x /= len;
+            dir.y /= len;
+        }
+        return dir;
+    }
+
+private:
+    GLFWwindow* window_;
+    ImVec2 win_pos_{100, 100};
+    float speed_{1.f};
+};
 
 int main() {
     // Initialize GLFW
@@ -33,6 +96,8 @@ int main() {
     // Set up Dear ImGui style
     ImGui::StyleColorsDark();
 
+    TestApp app(window);
+
     // Initialize ImGui for GLFW and OpenGL3
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 130"); // Adjust GLSL version as needed
@@ -46,8 +111,9 @@ int main() {
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // Show demo window
-        ImGui::ShowDemoWindow();
+        // Draw interface
+        app.Update(1.f);
+        app.Draw();
 
         // Rendering
         ImGui::Render();
